a17415675d1846f8997d58493aab8934365cb024
Gowtham Sivaji <gowtham.sivaji@ivygdcad.ivy-support.com>
2024-02-27 08:57:58 +0530

    Added remote webdriver

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.aspectj.util.FileUtil;
@@ -13,7 +15,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.exception.FrameworkException;
@@ -24,7 +26,7 @@ public class DriverFactory {
 	public Properties prop;
 	public OptionsManager optionsManager;
 	public static String highlight;
-	
+
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 
 	// We cannot keep initDriver method as static and declaring it as static will
@@ -36,8 +38,9 @@ public class DriverFactory {
 	 * 
 	 * @param browserName
 	 * @return this return the driver
+	 * @throws MalformedURLException 
 	 */
-	public WebDriver initDriver(Properties prop) {
+	public WebDriver initDriver(Properties prop){
 		
 		highlight = prop.getProperty("highlight").trim();
 		
@@ -47,16 +50,33 @@ public class DriverFactory {
 		System.out.println("Browser name is : " + browserName);
 
 		if (browserName.trim().equalsIgnoreCase("chrome")) {
-			//driver = new ChromeDriver(optionsManager.getChromeOptions());
-			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
-		} else if (browserName.equalsIgnoreCase("firefox")) {
-			//driver = new FirefoxDriver(optionsManager.getfirefoxOptions());
+			
+			if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+				// Remote execution
+				init_remoteDriver("chrome");
+			}
+			else {
+				// Local execution
+				tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+			}
+		} 
+		// FIREFOX
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver("firefox");
+			}
+			else {
 			tlDriver.set(new FirefoxDriver(optionsManager.getfirefoxOptions()));
-		} else if (browserName.equalsIgnoreCase("safari")) {
-			driver = new SafariDriver();
-		} else if (browserName.equalsIgnoreCase("edge")) {
-			//driver = new EdgeDriver(optionsManager.getEdgeOptions());
+			}
+		} 
+		// EDGE
+		else if (browserName.equalsIgnoreCase("edge")) {
+			if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver("edge");
+			}
+			else {
 			tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
+			}
 		} else {
 			System.out.println("Pls pass the right browser name..." + browserName);
 			throw new FrameworkException("NO BROWSER FOUND EXCEPTION....");
@@ -68,9 +88,39 @@ public class DriverFactory {
 
 		return getDriver();
 	}
+		/**
+		 * This method is called internally to initialise the driver with RemoteWebDriver
+		 */
+	private void init_remoteDriver(String browser) {
+		
+		System.out.println("Running tests on grid server ::::"+browser);
+		//run on remote
+		try {
+		switch (browser) {
+		
+		case "chrome":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getChromeOptions()));
+			break;
+			
+		case "firefox":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getfirefoxOptions()));
+			break;
+			
+		case "edge":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getEdgeOptions()));
+			break;
+
+		default:
+			System.out.println("Plz pass the right browser for remote execution..."+browser);
+			throw new FrameworkException("NOREMOTEBROWSEREXCEPTION");
+		}}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}}
+
 	/*
-	 * get the local thread copy of the driver
-	 * synchronized here every thread will get their individual copy
+	 * get the local thread copy of the driver synchronized here every thread will
+	 * get their individual copy
 	 */
 	public synchronized static WebDriver getDriver() {
 		return tlDriver.get();
@@ -83,39 +133,38 @@ public class DriverFactory {
 	 */
 
 	public Properties initProp() {
-		
-		//mvn clean install -Denv="stage"
+
+		// mvn clean install -Denv="stage"
 		// -D is used for environment variables
 		prop = new Properties();
-		FileInputStream ip=null;
+		FileInputStream ip = null;
 		String envName = System.getProperty("env");
-		System.out.println("Running test cases on env : "+envName);
-		try {	
-		if(envName==null) {
-			System.out.println("No env is passed...Running tests on QA env");
-			ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
-		}
-		else {
-			switch (envName.toLowerCase().trim()) {
-			case "qa":
+		System.out.println("Running test cases on env : " + envName);
+		try {
+			if (envName == null) {
+				System.out.println("No env is passed...Running tests on QA env");
 				ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
-				break;
-			case "stage":
-				ip = new FileInputStream("./src/test/resources/config/stage.config.properties");
-				break;
-			case "dev":
-				ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
-				break;
-			case "prod":
-				ip = new FileInputStream("./src/test/resources/config/config.properties");
-				break;
-			default:
-				System.out.println("Wrong env is passed...No need to run the test cases");
-				throw new FrameworkException("WRONG ENV IS PASSED.....");
-				//break;
+			} else {
+				switch (envName.toLowerCase().trim()) {
+				case "qa":
+					ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
+					break;
+				case "stage":
+					ip = new FileInputStream("./src/test/resources/config/stage.config.properties");
+					break;
+				case "dev":
+					ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
+					break;
+				case "prod":
+					ip = new FileInputStream("./src/test/resources/config/config.properties");
+					break;
+				default:
+					System.out.println("Wrong env is passed...No need to run the test cases");
+					throw new FrameworkException("WRONG ENV IS PASSED.....");
+				// break;
+				}
 			}
-		}} 
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		try {
@@ -127,13 +176,13 @@ public class DriverFactory {
 	}
 	// ./ mentioned it will go to the current project directory
 	// file input stream Used to interact with the files
-	
+
 	/**
 	 * take screenshot
 	 */
 	public static String getScreenshot() {
-		File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
-		String path = System.getProperty("user.dir")+"/screenshot/"+System.currentTimeMillis()+".png";
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
 		// user.dir will give the current directory
 		File destination = new File(path);
 		try {
@@ -141,13 +190,9 @@ public class DriverFactory {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		return path;
-		
-		
-	}
-	
-	
-	
-}
 
+		return path;
+
+	}
+
+}
--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
@@ -27,11 +28,17 @@ public class BaseTest {
 	
 	protected SoftAssert softAssert;
 	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browserName) {
 		df = new DriverFactory();
 		// The below init method will be called and driver is initalized from the driver factory
 		prop = df.initProp();
+		
+		if(browserName!=null) {
+			prop.setProperty("browser", browserName);
+		}
+		
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 		
