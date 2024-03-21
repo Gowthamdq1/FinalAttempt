2fd74bc76de2830f706574e38c71a1858f3652f9
Gowtham Sivaji <gowtham.sivaji@ivygdcad.ivy-support.com>
2024-03-21 08:11:30 +0530

    Added log4j

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -1,7 +1,9 @@
 package com.qa.opencart.tests;
 
+import org.apache.log4j.MDC;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 import com.qa.opencart.base.BaseTest;
 import com.qa.opencart.constants.AppConstants;
@@ -15,12 +17,18 @@ import io.qameta.allure.Story;
 @Epic("EPIC-100: design login for open cart app")
 @Story("US-Login: 101: design login page features for open cart")
 public class LoginPageTest extends BaseTest {
+	
+	private final Logger logger = Logger.getLogger(LoginPageTest.class);
 
 	@Severity(SeverityLevel.TRIVIAL)
 	@Description("Checking the title of the page..Author : Naveen")
 	@Test(priority = 1)
 	public void loginPageTitleTest() {
+		MDC.put("testClassName", this.getClass().getSimpleName());
+		logger.info("This is a log message from loginPageTitleTest");
+		
 		String actualTitle = loginPage.getLoginPageTitle();
+		logger.info("actual login page title : "+actualTitle);
 		Assert.assertEquals(actualTitle, AppConstants.LOGIN_PAGE_TITLE_VALUE);
 	}
 	@Severity(SeverityLevel.NORMAL)
